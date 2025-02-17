ca4591f8ada68f85dc6724ef4fe87075b32192fe
tien-tq200540 <tqtien157202@gmail.com>
2025-02-17 23:48:09 +0700

    Create rotateSpeed for ObjectMovement

--- Assets/_Data/Object/ObjMovement.cs
+++ Assets/_Data/Object/ObjMovement.cs
@@ -6,6 +6,7 @@ public class ObjMovement : TienMonoBehaviour
 {
     [SerializeField] protected Vector3 targetPosition;
     [SerializeField] protected float speed = 0.01f;
+    [SerializeField] protected float rotSpeed = 3f;
     [SerializeField] protected float distance = 1f;
     [SerializeField] protected float minDistance = 1f;
     protected virtual void FixedUpdate()
@@ -19,7 +20,12 @@ public class ObjMovement : TienMonoBehaviour
         Vector3 diff = this.targetPosition - transform.parent.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.parent.rotation = Quaternion.Euler(0f, 0f, rot_z);
+
+        float timeSpeed = this.rotSpeed * Time.fixedDeltaTime;
+        Quaternion targetEuler = Quaternion.Euler(0f, 0f, rot_z);
+        Quaternion currentEuler = Quaternion.Lerp(transform.parent.rotation, targetEuler, timeSpeed);
+
+        transform.parent.rotation = currentEuler;
     }
 
     protected virtual void Moving()
