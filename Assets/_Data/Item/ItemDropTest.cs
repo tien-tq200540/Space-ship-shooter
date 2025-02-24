a15bc02b0d5819ed94601cf41f127a2b28485d38
tien-tq200540 <tqtien157202@gmail.com>
2025-02-25 00:33:45 +0700

    Create ItemDropTest code

--- Assets/_Data/Item/ItemDropTest.cs
+++ Assets/_Data/Item/ItemDropTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class ItemDropTest : TienMonoBehaviour
 {
     public JunkCtrl junkCtrl;
+    public int dropCount = 0;
+    public List<ItemDropCount> dropCountItems = new();
 
     protected override void Start()
     {
@@ -14,7 +17,31 @@ public class ItemDropTest : TienMonoBehaviour
 
     protected virtual void Dropping()
     {
+        dropCount++;
         transform.GetPositionAndRotation(out Vector3 dropPos, out Quaternion dropRot);
-        ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObjectSO.dropList, dropPos, dropRot);
+        List<ItemDropRate> dropItems = ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObjectSO.dropList, dropPos, dropRot);
+
+        foreach (ItemDropRate itemDropRate in dropItems)
+        {
+            ItemDropCount item = dropCountItems.Find(i => i.itemName == itemDropRate.itemSO.itemName);
+            if (item == null)
+            {
+                item = new ItemDropCount();
+                item.itemName = itemDropRate.itemSO.itemName;
+                dropCountItems.Add(item);
+            }
+
+            item.count++;
+            item.rate = (float)Math.Round((float)item.count/this.dropCount, 2);
+        }
     }
+}
+
+[System.Serializable]
+public class ItemDropCount
+{
+    //For saving information about item and drop rate for testing
+    public string itemName = string.Empty;
+    public int count = 0;
+    public float rate;
 }
