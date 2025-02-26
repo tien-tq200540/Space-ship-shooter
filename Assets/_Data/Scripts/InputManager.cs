5db57bba6af06b49a9e47d4d69aa59f1c108a925
tien-tq200540 <tqtien157202@gmail.com>
2025-02-26 23:38:43 +0700

    Add input direction in InputManager

--- Assets/_Data/Scripts/InputManager.cs
+++ Assets/_Data/Scripts/InputManager.cs
@@ -13,6 +13,9 @@ public class InputManager : MonoBehaviour
     [SerializeField] protected float onFiring;
     public float OnFiring { get => onFiring; }
 
+    protected Vector4 direction;
+    public Vector4 Direction => direction;
+
     private void Awake()
     {
         if (instance != null) Debug.LogError("Only 1 InputManager allow to exist!");
@@ -22,6 +25,7 @@ public class InputManager : MonoBehaviour
     private void Update()
     {
         this.GetMouseDown();
+        this.GetDirectionByKeyDown();
     }
     private void FixedUpdate()
     {
@@ -36,5 +40,20 @@ public class InputManager : MonoBehaviour
     protected virtual void GetMouseDown()
     {
         onFiring = Input.GetAxis("Fire1");
+    }
+
+    protected virtual void GetDirectionByKeyDown()
+    {
+        this.direction.x = (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) ? 1 : 0; // Left
+        this.direction.y = (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) ? 1 : 0; // Right
+        this.direction.z = (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) ? 1 : 0; // Up
+        this.direction.w = (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) ? 1 : 0; // Down
+
+        
+        if (this.direction.x == 1) Debug.Log("Left");
+        if (this.direction.y == 1) Debug.Log("Right");
+        if (this.direction.z == 1) Debug.Log("Up");
+        if (this.direction.w == 1) Debug.Log("Down");
+        
     }
 }
