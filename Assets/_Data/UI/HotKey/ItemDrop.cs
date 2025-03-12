c45fb88815f756750da68777354e0dd6bc52a5c2
tien-tq200540 <tqtien157202@gmail.com>
2025-03-12 23:37:15 +0700

    Cannot drag item to a slot already has item

--- Assets/_Data/UI/HotKey/ItemDrop.cs
+++ Assets/_Data/UI/HotKey/ItemDrop.cs
@@ -7,6 +7,7 @@ public class ItemDrop : TienMonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
+        if (this.transform.childCount > 0) return;
         GameObject dropObj = eventData.pointerDrag;
         DragItem dragItem = dropObj.GetComponent<DragItem>();
         dragItem.RealParent = this.transform;
